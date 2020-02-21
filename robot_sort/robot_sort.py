7a3968d4eb59d8bb1e724ebd41c6f7e12d5ce65f
dominique maack <dominiquemaack@gmail.com>
2020-02-21 12:07:43 -0700

    still working through robot sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,38 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+  
+
+        while True:
+            self.set_light_off()
+            
+            while self.can_move_right():
+                self.move_right()
+                self.swap_item()
+                
+                if self.compare_item() == None or self.compare_item() > 0:
+                    self.swap_item()
+                    self.set_light_on()
+            if self.light_is_on() == False:      
+                return  
+            self.set_light_off() 
+
+            while self.can_move_left():
+                self.move_left()
+                self.swap_item()
+                
+                if self.compare_item() == None or self.compare_item() < 0:
+                    self.swap_item()
+                    self.set_light_on()
+                    # break
+            if self.light_is_on() == False:      
+                return      
+
+            
+                    
+
+            
+            
 
 
 if __name__ == "__main__":
